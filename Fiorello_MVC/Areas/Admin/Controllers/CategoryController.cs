c7b62aaf862f796b7f9cbbe04b2b430c64bf879a
Faha Tagizade <Fakhriyya16>
2024-05-20 23:36:17 +0400

    homework done

---
+++ Fiorello_MVC/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,75 @@
+﻿using Fiorello_MVC.Data;
+using Fiorello_MVC.Models;
+using Fiorello_MVC.Services.Interfaces;
+using Fiorello_MVC.ViewModels.Categories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fiorello_MVC.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private readonly ICategoryService _categoryService;
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            return View(await _categoryService.GetAllWithProductCountAsync());
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(CategoryCreateVM category)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            bool existCategory = await _categoryService.ExistAsync(category.Name);
+            if (existCategory)
+            {
+                ModelState.AddModelError("Name", "Category with this name already exists");
+                return View();
+            }
+            await _categoryService.CreateAsync(new Category { Name = category.Name });
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return BadRequest();
+            }
+
+            var category = await _categoryService.GetById((int)id);
+
+            if (category == null) { return NotFound(); }
+
+            await _categoryService.DeleteAsync(category);
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return BadRequest();
+            }
+            var category = await _categoryService.GetByIdWithProducts((int)id);
+            if (category == null) { return NotFound(); }
+            return View(category);
+        }
+    }
+}
+
